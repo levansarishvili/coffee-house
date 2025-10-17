97c3be535d2f34b041e87178650fd2233f7b118e
Levan Sarishvili <levanisarishvili322@gmail.com>
2025-10-17 12:48:38 +0400

    chore: initialize ypeScript, ESLint, and Vite configuration

---
+++ coffee-house/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  },
+};
