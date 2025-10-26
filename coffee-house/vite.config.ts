629a64b027ed2004c3f3166c4fbc58876e0b1c38
Levan Sarishvili <levanisarishvili322@gmail.com>
2025-10-26 13:50:21 +0400

    fix: update vite config to solve deployment error

--- coffee-house/vite.config.ts
+++ coffee-house/vite.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  // base: '/coffee-house/',
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
-        menu: 'menu.html',
-        register: 'register.html',
-        login: 'login.html',
-        cart: 'cart.html',
+        // menu: 'menu.html',
+        // register: 'register.html',
+        // login: 'login.html',
+        // cart: 'cart.html',
       },
     },
   },
