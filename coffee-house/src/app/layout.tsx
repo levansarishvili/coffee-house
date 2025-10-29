331cfbbfb30cc9151a9fe8e9dcc12dc09af94a37
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-10-29 15:26:59 +0400

    feat: initialize Next.js project and configuration

---
+++ coffee-house/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Coffee House",
+  description:
+    "Welcome to Coffee House, where every cup is brewed with love and care.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
--- coffee-house/src/types/cities.ts
+++
@@ -1,4 +0,0 @@
-export interface City {
-  city: string;
-  streets: string[];
-}
